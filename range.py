883a2e7af746d3087955762a354147618cebc717
Bojana-Mihajlovic <31313948+Bojana-Mihajlovic@users.noreply.github.com>
2021-11-11 15:36:57 +0100

    Add files via upload

---
+++ range.py
@@ -0,0 +1,16 @@
+
+def my_range(z,m):
+    y = z
+    output=[] 
+    while (y<m):
+        output.append(y)
+        y+=1
+    return output
+
+def one_number_range(x):  
+    y = 0
+    output1=[]
+    while (y<x):  
+       output1.append(y)
+       y+=1       
+    return output1
